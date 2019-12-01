30dbe91abd7c7831eb3fb6dafde5d23b70ab4015
wuhengzhen <wuhengzhen.qd@gmail.com>
2019-12-01 22:33:07 +0800

    submit

--- src/main/java/me/aaron/util/mybatis/generator/StartUpGenerator.java
+++ src/main/java/me/aaron/util/mybatis/generator/StartUpGenerator.java
@@ -3,13 +3,9 @@ package me.aaron.util.mybatis.generator;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.InvalidConfigurationException;
-import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,21 +22,33 @@ public class StartUpGenerator {
      * @author wuhengzhen
      * @date 2019/11/29 18:11
      **/
-    public static void main(String[] args) {
-        try {
-            System.out.println("--------------------start generator-------------------");
-            List<String> warnings = new ArrayList<>();
-            boolean overwrite = true;
-            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-            InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
-            ConfigurationParser cp = new ConfigurationParser(warnings);
-            Configuration config = cp.parseConfiguration(is);
-            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-            myBatisGenerator.generate(null);
-            System.out.println("--------------------end generator-------------------");
-        } catch (SQLException | IOException | InterruptedException | InvalidConfigurationException | XMLParserException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+        System.out.println("--------------------start generator-------------------");
+
+        StartUpGenerator app = new StartUpGenerator();
+
+        System.out.println(app.getClass().getResource("/").getPath());
+        app.generator();
+        System.out.println(System.getProperty("user.dir"));
+
+        System.out.println("--------------------end generator-------------------");
+
+    }
+
+
+    public void generator() throws Exception {
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(resourceAsStream);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+
+        for (String warning : warnings) {
+            System.out.println(warning);
         }
     }
 }
