ef2e4606c41d3e6b1a8ea358c6294cf5efef7f66
wuhengzhen <wuhengzhen.qd@gmail.com>
2020-01-10 15:43:35 +0800

    submit

---
+++ src/test/java/me/aaron/util/common/Tester.java
@@ -0,0 +1,21 @@
+package me.aaron.util.common;
+
+import org.junit.Test;
+
+/**
+ * @ClassName Tester
+ * @Description
+ * @Author wuhengzhen
+ * @Date 2020-01-07 14:58
+ * @Version 1.0
+ */
+public class Tester {
+
+    @Test
+    public void test1() {
+
+
+        System.out.println("test");
+    }
+
+}
